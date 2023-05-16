285c188c965dda804e0999ee0c89abced7fe84db
Tudor <tudorcristianmurariu@yahoo.com>
2023-05-16 23:07:11 +0300

    initializing

---
+++ src/main/java/com/example/AgentieDeTurismSpringBoot/AgentieDeTurismSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgentieDeTurismSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentieDeTurismSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentieDeTurismSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AgentieDeTurismSpringBoot/AgentieDeTurismSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgentieDeTurismSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentieDeTurismSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
